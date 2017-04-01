a3bd6f7c6a87fae0285d28da3e2d0ddc856ec99e
Claudia Yan <claudiab.yan@gmail.com>
2017-03-31 22:54:20 -0400

    added starter scripts and tile prefab

---
+++ Assets/Scripts/FieldGen.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldGen : MonoBehaviour
+{
+
+    public GameObject tile;
+    public int x = 22;
+    public int z = 10;
+    public int num_obstacles = 10;
+    private Matrix matrix;
+    private IList<GameObject> tilesList = new List<GameObject>();
+
+    // Use this for initialization
+    void Awake()
+    {
+        matrix = GetComponent<Matrix>();
+
+        matrix.InitializeMatrix(x, z);
+        InitializeField();
+
+    }
+
+    void Start()
+    {
+        SetObstacles();
+
+    }
+
+    private void SetObstacles()
+    {
+        var need_obs = num_obstacles;
+
+        if (num_obstacles < x + z)
+        {
+            for (int i = 0; i < need_obs; i++)
+            {
+                var tempx = Random.Range(0, x);
+                var tempz = Random.Range(0, z);
+                if (matrix.GetCell(tempx, tempz).visitstat != VisitStat.obstacle)
+                {
+                    matrix.SetCell(tempx, tempz, VisitStat.obstacle);
+                    var obs = getTile(tempx, tempz);
+                    obs.transform.localScale += new Vector3(0, 3, 0);
+                    obs.transform.parent = gameObject.transform;
+
+                }
+                else
+                    need_obs += 1;
+
+            }
+         
+
+        }
+    }
+
+    private GameObject getTile(int inputx, int inputz)
+    {
+        return tilesList[x + inputz * z];
+    }
+
+
+    private void InitializeField()
+    {
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < z; j++)
+            {
+                var single = Instantiate(tile);
+                single.transform.position = new Vector3(i, 0, j);
+                tilesList.Add(single);
+            }
+        }
+    }
+
+
+
+
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+
+
+}
---
+++ Assets/Scripts/Matrix.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum VisitStat
+{
+    unknown,
+    obstacle,
+    visited,
+    closed
+}
+
+public class Matrix : MonoBehaviour
+{
+    public class Cell
+    {
+
+        public VisitStat visitstat = VisitStat.unknown;
+        public float hn = float.MaxValue;
+        public float gn = float.MaxValue;
+
+        public float getFN()
+        {
+            return hn + gn;
+
+        }
+
+    }
+
+    private Cell[,] matrix;
+
+
+    public void InitializeMatrix(int x, int z)
+    {
+      
+        matrix = new Cell[x, z];
+
+    }
+
+    public void SetCell(int x, int z, VisitStat visitStat)
+    {
+        matrix[x, z].visitstat = visitStat;
+    }
+
+    public void SetCalcs(int x, int z, float hn, float gn)
+    {
+        matrix[x, z].hn = hn;
+        matrix[x, z].gn = gn;
+
+    }
+
+    public Cell GetCell(int x, int z)
+    {
+        Debug.LogFormat("-- {0}, {1}", x, z);
+        return matrix[x, z];
+
+    }
+
+
+  
+}
