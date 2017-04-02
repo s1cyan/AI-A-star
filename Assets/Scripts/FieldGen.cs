fc6244fe67628588049f088640c57c4d6f2361d3
Claudia Yan <claudiab.yan@gmail.com>
2017-04-01 23:38:25 -0400

    using a dictionary to keep track of tiles now

--- Assets/Scripts/FieldGen.cs
+++ Assets/Scripts/FieldGen.cs
@@ -2,70 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FieldGen : MonoBehaviour
-{
+public class FieldGen : MonoBehaviour {
 
+    public int x = 20;
+    public int z = 30;
     public GameObject tile;
-    public int x = 22;
-    public int z = 10;
-    public int num_obstacles = 10;
-    public Matrix m;
-    private IList<GameObject> tilesList = new List<GameObject>();
 
-    // Use this for initialization
-    void Start()
+    private Dictionary<Vector3, GameObject> tileDict = new Dictionary<Vector3, GameObject>();
+
+    void Awake()
     {
-        m.InitializeMatrix(x, z);
         InitializeField();
-        SetObstacles();
-
     }
 
-    private void SetObstacles()
-    {
-        var need_obs = num_obstacles;
-
-        if (num_obstacles < x + z)
-        {
-            for (int i = 0; i < need_obs; i++)
-            {
-                var tempx = Random.Range(0, x);
-                var tempz = Random.Range(0, z);
-                if (m.refMatrix[tempx,tempz] != null)
-                {
-                    //matrix.SetCell(tempx, tempz, VisitStat.obstacle);
-                    var obs = getTile(tempx, tempz);
-                    obs.transform.localScale += new Vector3(0, 5, 0);
-                }
-           
-            }
-         
-        }
-    }
-
-    private GameObject getTile(int inputx, int inputz)
-    {
-        return tilesList[inputx + inputz * z];
-    }
-
-
-    private void InitializeField()
+	private void InitializeField()
     {
         for (int i = 0; i < x; i++)
         {
             for (int j = 0; j < z; j++)
             {
-                var single = Instantiate(tile);
-                single.transform.position = new Vector3(i, 0, j);
-                tilesList.Add(single);
+                var initTile = Instantiate(tile);
+                initTile.transform.position = new Vector3(i, 0, j);
+                tileDict.Add(new Vector3(i, 0, j), initTile);
             }
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-		
-    }
-
 }
